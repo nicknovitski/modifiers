86735eaad342c3458fab1149168a2a7acaeb8681
Nick Novitski <nicknovitski@gmail.com>
2014-07-18 04:18:10 +0000

    walking skeleton, pending test

---
+++ lib/modifiers/deprecated.rb
@@ -0,0 +1,3 @@
+module Modifiers
+
+end
---
+++ spec/modifiers/deprecated_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'modifiers/deprecated'
+
+RSpec.describe Modifiers do
+  describe '#deprecated' do
+    it 'issues a warning'
+  end
+end
