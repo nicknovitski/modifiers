04c3a8a30b4409ffcd5442903d26d5e393a071a2
Nick Novitski <nicknovitski@gmail.com>
2014-07-19 18:37:42 +0000

    drop Modification subclasses

--- lib/modifiers/define_modifier.rb
+++ lib/modifiers/define_modifier.rb
@@ -1,9 +1,9 @@
 require 'modifiers/modification'
 
 module Modifiers
-  def self.define_modifier(name, modification_class = Modification, &block)
+  module_function def define_modifier(name, &block)
     define_method(name) do |sym|
-      mod = modification_class.new(klass: self, method: sym)
+      mod = Modification.new(klass: self, method: sym)
       define_method(sym) { |*args| mod.call(self, *args, &block) }
       send(mod.original_visibility, sym)
     end
--- lib/modifiers/deprecated.rb
+++ lib/modifiers/deprecated.rb
@@ -1,14 +1,8 @@
 require 'modifiers/define_modifier'
 
 module Modifiers
-  class Deprecation < Modification
-    def warning(invocation)
-      "deprecated method #{method_identifier} called from #{invocation.location.join(':')}"
-    end
-  end
-
-  define_modifier(:deprecated, Deprecation) do |invocation, modification|
-    warn modification.warning(invocation)
+  define_modifier(:deprecated) do |invocation|
+    warn "deprecated method #{invocation.method_identifier} called from #{invocation.location.join(':')}"
     invocation.invoke
   end
 end
