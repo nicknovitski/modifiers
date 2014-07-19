7b5327aa711aa51471310df0c1f3b53cfd59047d
Nick Novitski <nicknovitski@gmail.com>
2014-07-19 18:48:27 +0000

    enable RSpec Hipster Mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@ APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 $LOAD_PATH << File.join(APP_ROOT, 'lib')
 
 require 'shared_examples_for_modifiers'
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
