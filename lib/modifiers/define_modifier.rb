8a2cc0a6834903cab4018ad7057ea6847f7c5b9e
Nick Novitski <nicknovitski@gmail.com>
2014-07-19 19:53:39 +0000

    don't use module_function inline
    It breaks on 2.1.0. :/

--- lib/modifiers/define_modifier.rb
+++ lib/modifiers/define_modifier.rb
@@ -1,11 +1,12 @@
 require 'modifiers/modification'
 
 module Modifiers
-  module_function def define_modifier(name, &block)
+  def define_modifier(name, &block)
     define_method(name) do |sym|
       mod = Modification.new(klass: self, method: sym)
       define_method(sym) { |*args| mod.call(self, *args, &block) }
       send(mod.original_visibility, sym)
     end
   end
+  module_function :define_modifier
 end
