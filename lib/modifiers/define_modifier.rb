9558c583fe8ae61718e172e69dc85c0c31d644ac
Nick Novitski <nicknovitski@gmail.com>
2014-07-19 05:59:35 +0000

    extract Modifier class

--- lib/modifiers/define_modifier.rb
+++ lib/modifiers/define_modifier.rb
@@ -1,31 +1,13 @@
+require 'modifiers/modifier'
+
 module Modifiers
   def self.define_modifier(name, &block)
     define_method(name) do |sym|
-      original_method = instance_method(sym)
-      original_visibility = visibility_of(sym)
+      modifier = Modifier.new(self, sym)
       define_method(sym) do |*args|
-        bound_method = original_method.bind(self)
-        bound_method.define_singleton_method(:invoke) { bound_method.call(*args) }
-        if block
-          instance_exec(bound_method, *args, &block)
-        else
-          bound_method.invoke
-        end
+        modifier.call(self, *args, &block)
       end
-      send original_visibility, sym
-    end
-
-  end
-
-  private
-
-  def visibility_of(sym)
-    if private_method_defined?(sym)
-      :private
-    elsif protected_method_defined?(sym)
-      :protected
-    else
-      :public
+      send(modifier.original_visibility, sym)
     end
   end
 end
--- lib/modifiers/deprecated.rb
+++ lib/modifiers/deprecated.rb
@@ -1,7 +1,8 @@
 require 'modifiers/define_modifier'
+
 module Modifiers
   define_modifier(:deprecated) do |method|
-    caller[2] =~ /(.*?):(\d+).*?$/i
+    caller[3] =~ /(.*?):(\d+).*?$/i
     klass = self.kind_of? Module
     target = klass ? "#{self}." : "#{self.class}#"
     location = [$1, $2].join(":")
